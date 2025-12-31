8dd651ae50def450fd34dfcf711b2b36c768a67b
Utishnik <yrik.kokoyto.00@bk.ru>
2025-12-31 18:05:43 +0300

    more tests

--- tests/test_abstract_parser.rs
+++ tests/test_abstract_parser.rs
@@ -11,4 +11,34 @@ fn test() {
     }
     let r_unwrap: String = unsafe { r.unwrap_unchecked() };
     println!("test: {}", r_unwrap);
+
+    let test: String = "zxc test ".to_string();
+    let r: Result<String, obsidian_sync_lib::Parser::ParseExprError> =
+        String::parse_value(&test, &mut 5);
+    if r.is_err() {
+        println!("is err");
+        assert!(true);
+    }
+    let r_unwrap: String = unsafe { r.unwrap_unchecked() };
+    println!("test: {}", r_unwrap);
+
+    let test: String = "аааа? ".to_string();
+    let r: Result<String, obsidian_sync_lib::Parser::ParseExprError> =
+        String::parse_value(&test, &mut 2);
+    if r.is_err() {
+        println!("is err");
+        assert!(true);
+    }
+    let r_unwrap: String = unsafe { r.unwrap_unchecked() };
+    println!("test: {}", r_unwrap);
+
+    let test: String = "122аааа12+21 ".to_string();
+    let r: Result<String, obsidian_sync_lib::Parser::ParseExprError> =
+        String::parse_value(&test, &mut 2);
+    if r.is_err() {
+        println!("is err");
+        assert!(true);
+    }
+    let r_unwrap: String = unsafe { r.unwrap_unchecked() };
+    println!("test: {}", r_unwrap);
 }
