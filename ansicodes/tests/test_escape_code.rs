021765ca35739f4317f8863fbf006debacdf3dd3
Utishnik <yrik.kokoyto.00@bk.ru>
2026-01-01 21:46:19 +0300

    разделил тесты

---
+++ ansicodes/tests/test_escape_code.rs
@@ -0,0 +1,44 @@
+
+extern crate std;
+
+mod tests {
+    use std::{io::Write, string::String, vec::Vec};
+
+    macro_rules! assert_escape_output {
+        ($name:ident, $code:expr, $expected:expr) => {
+            #[test]
+            fn $name() {
+                let mut buf = Vec::new();
+                write!(buf, "{}", $code).unwrap();
+
+                let result = String::from_utf8(buf).unwrap();
+                assert_eq!(result, $expected);
+            }
+        };
+    }
+
+    assert_escape_output!(cursor_up_1, super::CursorUp(1), "\x1B[1A");
+    assert_escape_output!(cursor_up_23, super::CursorUp(23), "\x1B[23A");
+
+    assert_escape_output!(cursor_down_1, super::CursorDown(1), "\x1B[1B");
+    assert_escape_output!(cursor_down_23, super::CursorDown(23), "\x1B[23B");
+
+    assert_escape_output!(cursor_forward_1, super::CursorForward(1), "\x1B[1C");
+    assert_escape_output!(cursor_forward_23, super::CursorForward(23), "\x1B[23C");
+
+    assert_escape_output!(cursor_backward_1, super::CursorBackward(1), "\x1B[1D");
+    assert_escape_output!(cursor_backward_23, super::CursorBackward(23), "\x1B[23D");
+
+    assert_escape_output!(cursor_left, super::CursorLeft, "\x1B[1000D");
+    assert_escape_output!(cursor_save_position, super::CursorSavePosition, "\x1B[s");
+    assert_escape_output!(cursor_restore_position, super::CursorRestorePosition, "\x1B[u");
+    assert_escape_output!(cursor_get_position, super::CursorGetPosition, "\x1B[6n");
+    assert_escape_output!(cursor_next_line, super::CursorNextLine, "\x1B[E");
+    assert_escape_output!(cursor_prev_line, super::CursorPrevLine, "\x1B[F");
+    assert_escape_output!(cursor_hide, super::CursorHide, "\x1B[?25l");
+    assert_escape_output!(cursor_show, super::CursorShow, "\x1B[?25h");
+
+    assert_escape_output!(erase_lines_1, super::EraseLines(1), "\x1B[1000D\x1B[K");
+    assert_escape_output!(erase_lines_2, super::EraseLines(2), "\x1B[1000D\x1B[K\x1B[1A\x1B[1000D\x1B[K");
+    assert_escape_output!(erase_lines_3, super::EraseLines(3), "\x1B[1000D\x1B[K\x1B[1A\x1B[1000D\x1B[K\x1B[1A\x1B[1000D\x1B[K");
+}
