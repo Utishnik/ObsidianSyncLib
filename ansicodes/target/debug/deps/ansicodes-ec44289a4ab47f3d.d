3caaca6ce748b9427ff92d96a9322b2c3c603ddb
Utishnik <yrik.kokoyto.00@bk.ru>
2026-01-01 21:53:44 +0300

    чуть чуть поломал коммит

---
+++ ansicodes/target/debug/deps/ansicodes-ec44289a4ab47f3d.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/utishnik/Desktop/obsidian_sync_lib/ansicodes/target/debug/deps/ansicodes-ec44289a4ab47f3d.d: src/lib.rs
+
+/mnt/c/Users/utishnik/Desktop/obsidian_sync_lib/ansicodes/target/debug/deps/libansicodes-ec44289a4ab47f3d.rmeta: src/lib.rs
+
+src/lib.rs:
