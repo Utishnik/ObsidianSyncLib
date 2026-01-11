0fe08eabe405c5c4c4c282ed9a1ea110440b4b70
Utishnik <yrik.kokoyto.00@bk.ru>
2026-01-11 23:31:19 +0300

    refactoring

--- barrier_not_idle/src/zerotrait.rs
+++ barrier_not_idle/src/zerotrait.rs
@@ -1,58 +1,39 @@
+#[allow(missing_docs)]
 pub trait Zero {
+    #[doc = "просто для каждого типа опредляет метод zero() которые возвращет ноль его типа"]
     fn zero() -> Self;
 }
-impl Zero for u8 {
-    fn zero() -> Self {
-        0
+
+macro_rules! zero_decl {
+    ($ret_type: ty) => {
+    impl Zero for $ret_type{
+        fn zero() -> Self {
+            0
+        }
     }
+    };
 }
-impl Zero for u16 {
-    fn zero() -> Self {
-        0
+macro_rules! float_zero_decl {
+    ($ret_type: ty) => {
+    impl Zero for $ret_type{
+        fn zero() -> Self {
+            0.0
+        }
     }
+    };
 }
-impl Zero for u32 {
-    fn zero() -> Self {
-        0
-    }
-}
-impl Zero for u64 {
-    fn zero() -> Self {
-        0
-    }
-}
-impl Zero for usize {
-    fn zero() -> Self {
-        0
-    }
-}
-impl Zero for u128 {
-    fn zero() -> Self {
-        0
-    }
-}
-impl Zero for i8 {
-    fn zero() -> Self {
-        0
-    }
-}
-impl Zero for i16 {
-    fn zero() -> Self {
-        0
-    }
-}
-impl Zero for i32 {
-    fn zero() -> Self {
-        0
-    }
-}
-impl Zero for i64 {
-    fn zero() -> Self {
-        0
-    }
-}
-impl Zero for i128 {
-    fn zero() -> Self {
-        0
-    }
-}
+zero_decl!(u8);
+zero_decl!(u16);
+zero_decl!(u32);
+zero_decl!(u64);
+zero_decl!(u128);
+zero_decl!(usize);
+zero_decl!(i8);
+zero_decl!(i16);
+zero_decl!(i32);
+zero_decl!(i64);
+zero_decl!(i128);
+//zero_decl!(f16); // unstable
+float_zero_decl!(f32);
+float_zero_decl!(f64);
+//zero_decl!(f128); //unstable
