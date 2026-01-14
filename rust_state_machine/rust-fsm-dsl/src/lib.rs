e052d88cc66de2922e534a6ad384f1dd52436987
Utishnik <yrik.kokoyto.00@bk.ru>
2026-01-15 00:16:48 +0300

    add state machine

---
+++ rust_state_machine/rust-fsm-dsl/src/lib.rs
@@ -0,0 +1,212 @@
+//! DSL implementation for defining finite state machines for `rust-fsm`. See
+//! more in the `rust-fsm` crate documentation.
+
+#![recursion_limit = "128"]
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use std::{collections::BTreeSet, iter::FromIterator};
+use syn::{parse_macro_input, Attribute, Ident};
+
+mod parser;
+
+/// The full information about a state transition. Used to unify the
+/// represantion of the simple and the compact forms.
+struct Transition<'a> {
+    initial_state: &'a Ident,
+    input_value: &'a Ident,
+    final_state: &'a Ident,
+    output: &'a Option<Ident>,
+}
+
+fn attrs_to_token_stream(attrs: Vec<Attribute>) -> proc_macro2::TokenStream {
+    let attrs = attrs.into_iter().map(ToTokens::into_token_stream);
+    proc_macro2::TokenStream::from_iter(attrs)
+}
+
+#[proc_macro]
+/// Produce a state machine definition from the provided `rust-fmt` DSL
+/// description.
+pub fn state_machine(tokens: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(tokens as parser::StateMachineDef);
+
+    let doc = attrs_to_token_stream(input.doc);
+    let attrs = attrs_to_token_stream(input.attributes);
+
+    if input.transitions.is_empty() {
+        let output = quote! {
+            compile_error!("rust-fsm: at least one state transition must be provided");
+        };
+        return output.into();
+    }
+
+    let fsm_name = input.name;
+    let visibility = input.visibility;
+
+    let transitions = input.transitions.iter().flat_map(|def| {
+        def.transitions.iter().map(move |transition| Transition {
+            initial_state: &def.initial_state,
+            input_value: &transition.input_value,
+            final_state: &transition.final_state,
+            output: &transition.output,
+        })
+    });
+
+    let mut states = BTreeSet::new();
+    let mut inputs = BTreeSet::new();
+    let mut outputs = BTreeSet::new();
+    let mut transition_cases = Vec::new();
+    let mut output_cases = Vec::new();
+
+    #[cfg(feature = "diagram")]
+    let mut mermaid_diagram = format!(
+        "///```mermaid\n///stateDiagram-v2\n///    [*] --> {}\n",
+        input.initial_state
+    );
+
+    states.insert(&input.initial_state);
+
+    for transition in transitions {
+        let Transition {
+            initial_state,
+            final_state,
+            input_value,
+            output,
+        } = transition;
+
+        #[cfg(feature = "diagram")]
+        mermaid_diagram.push_str(&format!(
+            "///    {initial_state} --> {final_state}: {input_value}"
+        ));
+
+        transition_cases.push(quote! {
+            (Self::State::#initial_state, Self::Input::#input_value) => {
+                Some(Self::State::#final_state)
+            }
+        });
+
+        if let Some(output_value) = output {
+            output_cases.push(quote! {
+                (Self::State::#initial_state, Self::Input::#input_value) => {
+                    Some(Self::Output::#output_value)
+                }
+            });
+
+            #[cfg(feature = "diagram")]
+            mermaid_diagram.push_str(&format!(" [{output_value}]"));
+        }
+
+        #[cfg(feature = "diagram")]
+        mermaid_diagram.push('\n');
+
+        states.insert(initial_state);
+        states.insert(final_state);
+        inputs.insert(input_value);
+        if let Some(ref output) = output {
+            outputs.insert(output);
+        }
+    }
+
+    #[cfg(feature = "diagram")]
+    mermaid_diagram.push_str("///```");
+    #[cfg(feature = "diagram")]
+    let mermaid_diagram: proc_macro2::TokenStream = mermaid_diagram.parse().unwrap();
+
+    let initial_state_name = &input.initial_state;
+
+    let (input_type, input_impl) = match input.input_type {
+        Some(t) => (quote!(#t), quote!()),
+        None => (
+            quote!(Input),
+            quote! {
+                #attrs
+                pub enum Input {
+                    #(#inputs),*
+                }
+            },
+        ),
+    };
+
+    let (state_type, state_impl) = match input.state_type {
+        Some(t) => (quote!(#t), quote!()),
+        None => (
+            quote!(State),
+            quote! {
+                #attrs
+                pub enum State {
+                    #(#states),*
+                }
+            },
+        ),
+    };
+
+    let (output_type, output_impl) = match input.output_type {
+        Some(t) => (quote!(#t), quote!()),
+        None => {
+            // Many attrs and derives may work incorrectly (or simply not work) for empty enums, so we just skip them
+            // altogether if the output alphabet is empty.
+            let attrs = if outputs.is_empty() {
+                quote!()
+            } else {
+                attrs.clone()
+            };
+            (
+                quote!(Output),
+                quote! {
+                    #attrs
+                    pub enum Output {
+                        #(#outputs),*
+                    }
+                },
+            )
+        }
+    };
+
+    #[cfg(feature = "diagram")]
+    let diagram = quote! {
+        #[cfg_attr(doc, ::rust_fsm::aquamarine)]
+        #mermaid_diagram
+    };
+
+    #[cfg(not(feature = "diagram"))]
+    let diagram = quote!();
+
+    let output = quote! {
+        #doc
+        #diagram
+        #visibility mod #fsm_name {
+            #attrs
+            pub struct Impl;
+
+            pub type StateMachine = ::rust_fsm::StateMachine<Impl>;
+
+            #input_impl
+            #state_impl
+            #output_impl
+
+            impl ::rust_fsm::StateMachineImpl for Impl {
+                type Input = #input_type;
+                type State = #state_type;
+                type Output = #output_type;
+                const INITIAL_STATE: Self::State = Self::State::#initial_state_name;
+
+                fn transition(state: &Self::State, input: &Self::Input) -> Option<Self::State> {
+                    match (state, input) {
+                        #(#transition_cases)*
+                        _ => None,
+                    }
+                }
+
+                fn output(state: &Self::State, input: &Self::Input) -> Option<Self::Output> {
+                    match (state, input) {
+                        #(#output_cases)*
+                        _ => None,
+                    }
+                }
+            }
+        }
+    };
+
+    output.into()
+}
---
+++ rust_state_machine/rust-fsm-dsl/src/parser.rs
@@ -0,0 +1,196 @@
+use syn::{
+    braced, bracketed, parenthesized,
+    parse::{Error, Parse, ParseStream, Result},
+    token::{Bracket, Paren},
+    Attribute, Ident, Path, Token, Visibility,
+};
+
+/// The output of a state transition
+pub struct Output(Option<Ident>);
+
+impl Parse for Output {
+    fn parse(input: ParseStream) -> Result<Self> {
+        if input.lookahead1().peek(Bracket) {
+            let output_content;
+            bracketed!(output_content in input);
+            Ok(Self(Some(output_content.parse()?)))
+        } else {
+            Ok(Self(None))
+        }
+    }
+}
+
+impl From<Output> for Option<Ident> {
+    fn from(output: Output) -> Self {
+        output.0
+    }
+}
+
+/// Represents a part of state transition without the initial state. The `Parse`
+/// trait is implemented for the compact form.
+pub struct TransitionEntry {
+    pub input_value: Ident,
+    pub final_state: Ident,
+    pub output: Option<Ident>,
+}
+
+impl Parse for TransitionEntry {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let input_value = input.parse()?;
+        input.parse::<Token![=>]>()?;
+        let final_state = input.parse()?;
+        let output = input.parse::<Output>()?.into();
+        Ok(Self {
+            input_value,
+            final_state,
+            output,
+        })
+    }
+}
+
+/// Parses the transition in any of the possible formats.
+pub struct TransitionDef {
+    pub initial_state: Ident,
+    pub transitions: Vec<TransitionEntry>,
+}
+
+impl Parse for TransitionDef {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let initial_state = input.parse()?;
+        // Parse the transition in the simple format
+        // InitialState(Input) => ResultState [Output]
+        let transitions = if input.lookahead1().peek(Paren) {
+            let input_content;
+            parenthesized!(input_content in input);
+            let input_value = input_content.parse()?;
+            input.parse::<Token![=>]>()?;
+            let final_state = input.parse()?;
+            let output = input.parse::<Output>()?.into();
+
+            vec![TransitionEntry {
+                input_value,
+                final_state,
+                output,
+            }]
+        } else {
+            // Parse the transition in the compact format
+            // InitialState => {
+            //     Input1 => State1,
+            //     Input2 => State2 [Output]
+            // }
+            input.parse::<Token![=>]>()?;
+            let entries_content;
+            braced!(entries_content in input);
+
+            let entries: Vec<_> = entries_content
+                .parse_terminated(TransitionEntry::parse, Token![,])?
+                .into_iter()
+                .collect();
+            if entries.is_empty() {
+                return Err(Error::new_spanned(
+                    initial_state,
+                    "No transitions provided for a compact representation",
+                ));
+            }
+            entries
+        };
+        Ok(Self {
+            initial_state,
+            transitions,
+        })
+    }
+}
+
+/// Parses the whole state machine definition in the following form (example):
+///
+/// ```rust,ignore
+/// state_machine! {
+///     CircuitBreaker(Closed)
+///
+///     Closed(Unsuccessful) => Open [SetupTimer],
+///     Open(TimerTriggered) => HalfOpen,
+///     HalfOpen => {
+///         Successful => Closed,
+///         Unsuccessful => Open [SetupTimer]
+///     }
+/// }
+/// ```
+pub struct StateMachineDef {
+    pub doc: Vec<Attribute>,
+    /// The visibility modifier (applies to all generated items)
+    pub visibility: Visibility,
+    pub name: Ident,
+    pub initial_state: Ident,
+    pub transitions: Vec<TransitionDef>,
+    pub attributes: Vec<Attribute>,
+    pub input_type: Option<Path>,
+    pub state_type: Option<Path>,
+    pub output_type: Option<Path>,
+}
+
+impl Parse for StateMachineDef {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let mut state_machine_attributes = Vec::new();
+        let mut doc = Vec::new();
+        let attributes = Attribute::parse_outer(input)?
+            .into_iter()
+            .filter_map(|attribute| {
+                if attribute.path().is_ident("state_machine") {
+                    state_machine_attributes.push(attribute);
+                    None
+                } else if attribute.path().is_ident("doc") {
+                    doc.push(attribute);
+                    None
+                } else {
+                    Some(attribute)
+                }
+            })
+            .collect();
+
+        let mut input_type = None;
+        let mut state_type = None;
+        let mut output_type = None;
+
+        for attribute in state_machine_attributes {
+            attribute.parse_nested_meta(|meta| {
+                let content;
+                parenthesized!(content in meta.input);
+                let p: Path = content.parse()?;
+
+                if meta.path.is_ident("input") {
+                    input_type = Some(p);
+                } else if meta.path.is_ident("state") {
+                    state_type = Some(p);
+                } else if meta.path.is_ident("output") {
+                    output_type = Some(p);
+                }
+
+                Ok(())
+            })?;
+        }
+
+        let visibility = input.parse()?;
+        let name = input.parse()?;
+
+        let initial_state_content;
+        parenthesized!(initial_state_content in input);
+        let initial_state = initial_state_content.parse()?;
+
+        let transitions = input
+            .parse_terminated(TransitionDef::parse, Token![,])?
+            .into_iter()
+            .collect();
+
+        Ok(Self {
+            doc,
+            visibility,
+            name,
+            initial_state,
+            transitions,
+            attributes,
+            input_type,
+            state_type,
+            output_type,
+        })
+    }
+}
