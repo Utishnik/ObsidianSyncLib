b27a95f927a9801f72df2a07328c88bd7905dfb4
Utishnik <yrik.kokoyto.00@bk.ru>
2026-01-27 00:17:12 +0300

    more

--- bitfields_c2rust/build_error/lib.rs
+++ bitfields_c2rust/build_error/lib.rs
@@ -19,15 +19,33 @@ mod unstable_build_error {
 }
 
 #[cfg(feature = "stable")]
-mod unstable_build_error {
+mod stable_build_error {
     use core::fmt::Display;
     use syn::DeriveInput;
     use syn::Error;
+    use syn::parse_macro_input;
 
     pub fn build_error_feature<U>(ast: &DeriveInput, message: U) -> Error
     where
         U: Display,
     {
         Error::new_spanned(ast, message)
+    }
+
+    pub fn build_position_error<U>(ast: &DeriveInput, message: U) -> Error
+    where
+        U: Display,
+    {
+        Error::new(ast.ident.span(), message)
+    }
+}
+
+#[cfg(feature = "comp_err")]
+mod build_compile_error {
+    use proc_macro::TokenStream;
+    use syn::DeriveInput;
+    use syn::Error;
+    pub fn build_error(ast: &DeriveInput, err: Error) -> TokenStream {
+        err.to_compile_error().into()
     }
 }
