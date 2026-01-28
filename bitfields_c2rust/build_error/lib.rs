32cf7a40fd26189878437c76fb03b4075cdbcf3b
Utishnik <yrik.kokoyto.00@bk.ru>
2026-01-29 00:43:45 +0300

    todo fixgit add .!

--- bitfields_c2rust/build_error/lib.rs
+++ bitfields_c2rust/build_error/lib.rs
@@ -3,6 +3,8 @@ use proc_macro2::Span;
 use syn::DeriveInput;
 use syn::Error;
 use syn::parse_macro_input;
+use syn::Meta;
+use syn::Lit;
 
 #[cfg(feature = "unstable")]
 mod unstable_build_error {
@@ -66,7 +68,43 @@ mod build_compile_error {
 pub fn build_compiler_error(ast: TokenStream, err: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(ast);
     let err_convert: Result<syn::ItemMacro, Error> = syn::parse(err);
-    let err_convert_unwrap: Error = err_convert.unwrap_err();
-    let res: TokenStream = build_compile_error::build_error(&input, err_convert_unwrap);
-    res
+    if err_convert.is_err(){
+        let err_convert_unwrap: Error = unsafe{err_convert.unwrap_err_unchecked()};
+        let res: TokenStream = build_compile_error::build_error(&input, err_convert_unwrap);
+        res
+    }
+    else{
+        let empty: TokenStream = TokenStream::new();
+        empty
+    }
+}
+
+//нужно парсинг всех типов
+#[cfg(feature = "comp_err")]
+#[proc_macro_derive(err_feature, attributes(msg))]
+pub fn build_error_feature(ast: TokenStream) -> TokenStream{
+    use core::any::{Any, TypeId};
+    let message: DeriveInput = parse_macro_input!(ast as DeriveInput);
+    let attrl: Vec<Box<dyn Any>> = Vec::new();
+    message.attrs
+        .iter()
+        .filter(|attr| attr.path().is_ident("msg"))
+        .filter(|attr| {
+            match attr.parse_args::<Meta>() {
+                Ok(Meta::NameValue(name_value)) => {
+                    // Используем name_value.value вместо name_value.lit
+                    match &name_value.value {
+                        syn::Expr::Lit(expr_lit) => {
+                            if let syn::Lit::Str(lit_str) = &expr_lit.lit {
+                                attrl.push(Box::new(lit_str.value()));
+                        }
+                        _ => ,
+                    }
+                }
+                _ => {},
+            }
+        }
+    });
+    let input: DeriveInput = parse_macro_input!(ast);
+    stable_build_error::build_position_error(&input, message);
 }
