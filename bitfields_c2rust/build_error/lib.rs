7888965b0c8e77f345fbc2ea5917f99e2fa7faea
Utishnik <yrik.kokoyto.00@bk.ru>
2026-01-29 23:56:29 +0300

    add build_error_feature ...

--- bitfields_c2rust/build_error/lib.rs
+++ bitfields_c2rust/build_error/lib.rs
@@ -2,9 +2,15 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use syn::DeriveInput;
 use syn::Error;
-use syn::parse_macro_input;
-use syn::Meta;
 use syn::Lit;
+use syn::Meta;
+use syn::parse_macro_input;
+
+macro_rules! bug_match {
+    () => {
+        panic!("pattern match err")
+    };
+}
 
 #[cfg(feature = "unstable")]
 mod unstable_build_error {
@@ -58,7 +64,7 @@ mod build_compile_error {
     use proc_macro::TokenStream;
     use syn::DeriveInput;
     use syn::Error;
-    pub fn build_error(ast: &DeriveInput, err: Error) -> TokenStream {
+    pub fn build_error(err: Error) -> TokenStream {
         err.to_compile_error().into()
     }
 }
@@ -66,30 +72,29 @@ mod build_compile_error {
 #[cfg(feature = "comp_err")]
 #[proc_macro_attribute]
 pub fn build_compiler_error(ast: TokenStream, err: TokenStream) -> TokenStream {
-    let input: DeriveInput = parse_macro_input!(ast);
     let err_convert: Result<syn::ItemMacro, Error> = syn::parse(err);
-    if err_convert.is_err(){
-        let err_convert_unwrap: Error = unsafe{err_convert.unwrap_err_unchecked()};
-        let res: TokenStream = build_compile_error::build_error(&input, err_convert_unwrap);
+    if err_convert.is_err() {
+        let err_convert_unwrap: Error = unsafe { err_convert.unwrap_err_unchecked() };
+        let res: TokenStream = build_compile_error::build_error(err_convert_unwrap);
         res
-    }
-    else{
+    } else {
         let empty: TokenStream = TokenStream::new();
         empty
     }
 }
 
-//нужно парсинг всех типов
 #[cfg(feature = "comp_err")]
 #[proc_macro_derive(err_feature, attributes(msg))]
-pub fn build_error_feature(ast: TokenStream) -> TokenStream{
+pub fn build_error_feature(ast: TokenStream) -> TokenStream {
     use core::any::{Any, TypeId};
-    let message: DeriveInput = parse_macro_input!(ast as DeriveInput);
-    let attrl: Vec<Box<dyn Any>> = Vec::new();
-    message.attrs
+    let clone_ast: TokenStream = ast.clone();
+    let message: DeriveInput = parse_macro_input!(clone_ast as DeriveInput);
+    let mut attrl: Vec<Box<dyn Any>> = Vec::new();
+    let _ = message
+        .attrs
         .iter()
         .filter(|attr| attr.path().is_ident("msg"))
-        .filter(|attr| {
+        .map(|attr| {
             match attr.parse_args::<Meta>() {
                 Ok(Meta::NameValue(name_value)) => {
                     // Используем name_value.value вместо name_value.lit
@@ -97,14 +102,33 @@ pub fn build_error_feature(ast: TokenStream) -> TokenStream{
                         syn::Expr::Lit(expr_lit) => {
                             if let syn::Lit::Str(lit_str) = &expr_lit.lit {
                                 attrl.push(Box::new(lit_str.value()));
+                            } else {
+                            }
                         }
-                        _ => ,
+                        _ => {
+                            dbg!("please convert to string");
+                        }
                     }
                 }
-                _ => {},
+                _ => {
+                    bug_match!()
+                }
             }
-        }
-    });
+        });
     let input: DeriveInput = parse_macro_input!(ast);
-    stable_build_error::build_position_error(&input, message);
+    let first_val: Option<&Box<dyn Any>> = attrl.first();
+    if first_val.is_none() {
+        panic!("build_error_feature not find attr!");
+    }
+    let first_val_unwrap: &Box<dyn Any> = unsafe { first_val.unwrap_unchecked() };
+    let first_val_downcast: Option<&String> = first_val_unwrap.downcast_ref::<String>();
+    if first_val_downcast.is_none() {
+        dbg!("please convert to string");
+        let empty: TokenStream = TokenStream::new();
+        return empty;
+    }
+    let first_val_downcast_unwrap: &String = unsafe { first_val_downcast.unwrap_unchecked() };
+    let res_err: Error =
+        stable_build_error::build_position_error(&input, first_val_downcast_unwrap);
+    build_compile_error::build_error(res_err)
 }
